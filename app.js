f77ef5288759228651485f76bc96142dc4e49fde
Shawn Nguru <nguru.shawn@student.greenriver.edu>
2025-02-05 15:52:35 -0600

    console logging information from the page

--- app.js
+++ app.js
@@ -3,16 +3,31 @@ import express from 'express';
 const app = express();
 
 app.use(express.static('public'));
-
+app.use(express.urlencoded({ extended: true }));
 const PORT = 3000;
 
+const contacts = [];
 
 app.get('/', (req, res) => {
     res.sendFile(`${import.meta.dirname}/views/index.html`);
 });
 
-app.get('/confirm', (req, res) => {
+app.post('/confirm', (req, res) => {
     res.sendFile(`${import.meta.dirname}/views/confirm.html`);
+    const contact = {
+        fname: req.body.fname,
+        lname: req.body.lname,
+        email: req.body.job,
+        address: req.body.address,
+        location: req.body.location,
+        other: req.body.other,
+        message: req.body.message,
+        mailing: req.body.mailing,
+        format: req.body.format,
+     };
+     contacts.push(contact);
+     console.log(contacts);
+     res.sendFile(`${import.meta.dirname}/views/confirm.html`);
  });
 
  app.get('/back', (req, res) => {
