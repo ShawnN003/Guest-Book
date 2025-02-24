fd45c23a8dc77ed9a5262ff24cd4b500edd2904c
Shawn Nguru <nguru.shawn@student.greenriver.edu>
2025-02-24 10:53:33 -0800

    Finished criteria

--- app.js
+++ app.js
@@ -13,46 +13,47 @@ const PORT = 3000;
 const contacts = [];
 
 app.get('/', (req, res) => {
-    res.sendFile(`${import.meta.dirname}/views/index.html`);
+    res.render('index');
 });
 
 app.post('/confirm', (req, res) => {
     const contact = {
         fname: req.body.fname,
         lname: req.body.lname,
-        email: req.body.job,
+        email: req.body.email,
         address: req.body.address,
         location: req.body.location,
         other: req.body.other,
         message: req.body.message,
         mailing: req.body.mailing,
         format: req.body.format,
+        timestamp: new Date()
      };
      contacts.push(contact);
-     res.sendFile(`${import.meta.dirname}/views/confirm.html`);
+     if(req.body.fname == "" || req.body.lname =="" || req.body.email == "")
+     {
+        res.send('INVALID INPUT');
+     }
+     else 
+     {
+        res.render('confirm', { contact });
+     }
  });
 
 
 
-app.get('/admin/contacts', (req, res) => {
-    let html = '<h1>Contacts</h1><ul>';
-    for (const contact of contacts) {
-        html += `<li>${contact.fname} ${contact.lname} - ${contact.email} - ${contact.address} - ${contact.location} - 
-        ${contact.other} - ${contact.message} - ${contact.mailing} - ${contact.format}</li>`;
-    }
-    html += '</ul>';
-    res.send(html);
+app.get('/list', (req, res) => {
+    res.render('admin', { contacts });
 });
 
 
  app.get('/back', (req, res) => {
-    res.sendFile(`${import.meta.dirname}/views/index.html`);
+    res.render('index');
  });
 
- app.get('/back', (req, res) => {
-    res.sendFile(`${import.meta.dirname}/views/index.html`);
+ app.get('/contactList', (req, res) => {
+    res.render('/admin/contacts');
  });
-
 
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
